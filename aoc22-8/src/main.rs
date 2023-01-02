2ae6f11313566c7518a86197227d6efd0f54b954
Keith Kim <karmakaze@users.noreply.github.com>
2023-01-02 18:01:58 -0500

    ? for puzzle 8

--- aoc22-8/src/main.rs
+++ aoc22-8/src/main.rs
@@ -21,26 +21,13 @@ fn main() {
         forest.add_row(line);
     }
     println!("forest width {} x depth {}", forest.width, forest.depth);
-
-    // for y in 0..lines.len() {
-    //     for x in 0..width {
-    //         let height = forest.tree_height((x, y));
-    //         print!("{height} ");
-    //     }
-    //     println!();
-    // }
-
-    // let mut tallest_height: i32 = -1;
-    // let mut tallest_col: usize = 0;
-    // let mut tallest_row: usize = 0;
-    // forest.visit_trees(|col: usize, row: usize, tree_height: i32| {
-    //     if tree_height > tallest_height {
-    //         tallest_col = col;
-    //         tallest_row = row;
-    //         tallest_height = tree_height;
-    //     }
-    // });
-    // println!("tallest tree {tallest_height} at ({tallest_col}, {tallest_row})");
+    for row in 0..forest.depth {
+        for col in 0..forest.width {
+            let h = forest.tree_height((col, row));
+            print!("{h}");
+        }
+        println!();
+    }
 
     let mut visible_trees: HashMap<(usize, usize), i32> = HashMap::new();
 
@@ -87,6 +74,39 @@ fn main() {
         println!();
     }
     println!("Part 1: {} trees are visible from the outside.", visible_trees.len());
+
+    let mut max_score: i32 = -1;
+    let mut max_pos = (forest.width, forest.depth);
+    for row in 0..forest.depth {
+        for col in 0..forest.width {
+            let pos = (col, row);
+            let score = forest.scenic_score(pos, &mut |_, _| {});
+            if score > max_score {
+                max_score = score;
+                max_pos = pos;
+            }
+        }
+    }
+    println!("Part 2: Scenic score of {} visible from {:?}.", max_score, max_pos);
+
+    visible_trees.clear();
+    forest.scenic_score(max_pos, &mut |pos, tree_height| {
+        visible_trees.insert(pos, tree_height);
+    });
+    visible_trees.insert(max_pos, forest.tree_height(max_pos));
+
+    println!("Visible trees:");
+    for row in 0..forest.depth {
+        for col in 0..forest.width {
+            let pos = (col, row);
+            let ch = match visible_trees.get(&pos) {
+                Some(h) => format!("{}", h),
+                None => " ".to_string(),
+            };
+            print!("{ch}");
+        }
+        println!();
+    }
 }
 
 fn updater(tallest_visible: i32, tree_height: i32, pos: (usize, usize), mut visible_trees: &mut HashMap<(usize, usize), i32>) -> i32 {
@@ -100,6 +120,89 @@ fn updater(tallest_visible: i32, tree_height: i32, pos: (usize, usize), mut visi
 }
 
 impl Forest {
+    fn scenic_score(&self, house_pos: (usize, usize), seen: &mut dyn FnMut((usize, usize), i32) -> ()) -> i32 {
+        let mut updater = |house: i32, tree_height: i32, tree_pos: (usize, usize), mut seen_count: &mut i32, mut seen_height: &mut i32| -> bool {
+            if tree_height < house {
+                *seen_count += 1;
+                seen(tree_pos, tree_height);
+
+                if tree_height >= *seen_height {
+                    *seen_height = tree_height;
+                }
+                true
+            } else {
+                *seen_count += 1;
+                seen(tree_pos, tree_height);
+                false
+            }
+        };
+        let (house_col, house_row) = house_pos;
+        let house_height = self.tree_height(house_pos);
+        let mut score: i32 = 1;
+
+        let mut seen_count: i32 = 0;
+        let mut seen_height: i32 = -1;
+        if house_row > 0 {
+            for row in (0..=(house_row - 1)).rev() {
+                let tree_pos = (house_col, row);
+                let tree_height = self.tree_height(tree_pos);
+                if !updater(house_height, tree_height, tree_pos, &mut seen_count, &mut seen_height) {
+                    break;
+                }
+            }
+            score *= seen_count;
+        }
+        if house_pos == (22, 53) {
+            println!("Count {seen_count} looking up.");
+        }
+
+        seen_count = 0;
+        seen_height = -1;
+        for row in (house_row + 1)..self.depth {
+            let tree_pos = (house_col, row);
+            let tree_height = self.tree_height(tree_pos);
+            if !updater(house_height, tree_height, tree_pos, &mut seen_count, &mut seen_height) {
+                break;
+            }
+        }
+        score *= seen_count;
+        if house_pos == (22, 53) {
+            println!("Count {seen_count} looking down.");
+        }
+
+        if house_col > 0 {
+            seen_count = 0;
+            seen_height = -1;
+            for col in (0..=(house_col - 1)).rev() {
+                let tree_pos = (col, house_row);
+                let tree_height = self.tree_height(tree_pos);
+                if !updater(house_height, tree_height, tree_pos, &mut seen_count, &mut seen_height) {
+                    break;
+                }
+            }
+            score *= seen_count;
+            if house_pos == (22, 53) {
+                println!("Count {seen_count} looking left.");
+            }
+        }
+
+        seen_count = 0;
+        seen_height = -1;
+        for col in (house_col + 1)..self.width {
+            let tree_pos = (col, house_row);
+            let tree_height = self.tree_height(tree_pos);
+            if !updater(house_height, tree_height, tree_pos, &mut seen_count, &mut seen_height) {
+                break;
+            }
+        }
+        score *= seen_count;
+        if house_pos == (22, 53) {
+            println!("Count {seen_count} looking right.");
+        }
+
+        score
+    }
+
     fn add_row(&mut self, line: &str) {
         for c in line.chars() {
             let height = format!("{c}").parse::<i32>().unwrap();
